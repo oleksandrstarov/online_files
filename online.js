c05c6081ec530f510babeea4872695db5cf79e3c
Oleksandr <oleksandr.starov@gmail.com>
2016-09-30 08:41:29 +0000

    first commit!

--- online.js
+++ online.js
@@ -1,12 +1,21 @@
 var fileSystem = require('fs');
 
-var path = __dirname + '/online';
+//set own path
+var path = __dirname;
 console.log(path);
 
 fileSystem.watch(path, (eventType, filename) => {
   console.log(`event type is: ${eventType}`);
-  if (filename) {
+  if (filename && filename === 'index.htm') {
     console.log(`filename provided: ${filename}`);
+    require('simple-git')()
+         .add('./*')
+         .commit("first commit!")
+         .addRemote('live', 'https://github.com/oleksandrstarov/online.git')
+         .push(['-u', 'live', 'gh-pages'], function () {
+            console.log('DONE'); 
+         });
+    
   } else {
     console.log('filename not provided');
   }
