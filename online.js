c85b69795156acb3ddb082e2189b0f39e9bd98c2
Oleksandr Starov <Oleksandr Starov>
2016-10-02 16:44:19 +0300

    data

--- online.js
+++ online.js
@@ -1,24 +1,63 @@
-var fileSystem = require('fs'),
-    git = require('simple-git')();
+var fs = require('fs'),
+    git = require('simple-git')(),
+    iconv  = require('iconv-lite');
 
 //set own path
-var path = __dirname;
-console.log(path);
-
-//git.addRemote('online3', 'https://github.com/oleksandrstarov/online.git');
+var pathEnter = __dirname + '//data//ResultList.htm';
+var pathResult = __dirname + '//data//index.htm';
 
 
-fileSystem.watch(path, (eventType, filename) => {
-  console.log(`event type is: ${eventType}`);
-  if (filename && filename === 'index.htm') {
-    console.log(`filename provided: ${filename}`);
-        git.add(filename)
-         .commit("first commit!")
-         .push(['-u', 'https://github.com/oleksandrstarov/online.git', 'HEAD:gh-pages'], function () {
-            console.log('DONE'); 
-         });
-    
-  } else {
-    console.log('filename not provided');
-  }
+//git.addRemote('online', 'https://github.com/oleksandrstarov/online.git');
+fs.watchFile(pathEnter, (curr, prev) => {
+  fs.readFile(pathEnter, (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    var info = iconv.decode(data, "win1251");
+    fs.writeFile(pathResult, info, (err) => {
+      if (err) {
+          console.log(err);
+          return;
+      }
+      console.log('It\'s saved!');
+    });
+  });
 });
+
+fs.watchFile(pathResult, (curr, prev) => {
+  git.add(pathResult)
+   .commit("first commit!")
+   .push(['-u', 'online', 'HEAD:gh-pages'], function () {
+      console.log('DONE');
+   });
+  console.log(`finished`);
+});
+
+/*
+  console.log(curr, prev);
+  if (filename && filename === 'ResultList.htm') {
+    fs.open(path + '//ResultList.htm', 'r', (err, fd) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        console.error('myfile does not exist');
+        return;
+      } else {
+        throw err;
+      }
+    }
+    console.log(fd);
+  //readMyData(fd);
+  });
+
+  } else if (filename && filename === 'index.htm') {
+    console.log(`filename provided: ${filename}`);
+      git.add(path + '//index.htm')
+       .commit("first commit!")
+       .push(['-u', 'online', 'HEAD:gh-pages'], function () {
+          console.log('DONE');
+       });
+      console.log(`finished`);
+  }else {
+    console.log('filename not provided');
+  }*/
